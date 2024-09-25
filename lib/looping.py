a9ca926456f9c2a2d77af1cdaa4b2f87d70a049d
daniel nzioka <dmwinzi50@gmail.com>
2024-09-25 17:44:36 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,11 +2,21 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i=10
+    while(i>0):
+        print(f"{i}")
+        i -= 1
+    else:
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_lists=list()
+    for num in int_list:
+        new = num * num
+        int_lists.append(new)
+    return int_lists
+    
 
 def fizzbuzz():
     # code goes here!
