966f28e715237e434fcf2bbdc964dc79970c21f1
daniel nzioka <dmwinzi50@gmail.com>
2024-09-25 20:33:44 +0300

    fizzbuzz

--- lib/looping.py
+++ lib/looping.py
@@ -20,4 +20,17 @@ def square_integers(int_list):
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+        if(i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif(i % 3 == 0):
+            print("Fizz")
+        elif(i % 5 == 0):
+            print("Buzz")
+        else:
+            print(i)
+            
+    
+
+
+
